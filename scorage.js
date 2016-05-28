89cf3f3ee99352a98d3fe0c420c58d2565b819cf
Brodie Blackburn <brodie.blackburn@gmail.com>
2016-05-28 12:22:40 +1000

    Rewrite all the things
    Changes include
     - Changed structure of player and round data. Allows for simpler modification and templates.
     - Hands property introduced for each round. The number of hands in a round is not necessarily equal to the round number. Allows for proper Shing scoring.
     - Number of hands for next round is predicted based on if hands are increasing or decreasing.
     - Rules details reworded
     - Player names mapped to unique id. Allows for easy player reordering.
     - Local storage has a dataVersion attribute. Allows data structure changes in future.

--- scorage.js
+++ scorage.js
@@ -1,259 +1,57 @@
-Ractive.DEBUG = false;
+var latestDataVersion = 1,
+  dataVersion = store.get('dataVersion'),
+  view = store.get('view'),
+  rules = store.get('rules'),
+  playerMap = store.get('playerMap'),
+  playerOrder = store.get('playerOrder'),
+  rounds = store.get('rounds');
 
-// Retrieve data from local storage if present.
-var players = store.get('players');
-var rules = store.get('rules');
-var view = store.get('view');
-if (!players) {
-  console.log('No players in storage, using defaults.');
-  players = [{
-    name: 'Player 1',
-    bids: [null],
-    tricks: [null]
-  }];
-}
-if (!rules) {
-  console.log('No rules in storage, using default rules.');
-  rules = 'shing';
-}
-if (!view) {
-  console.log('No view in storage, using default view.');
+if (!dataVersion || dataVersion < latestDataVersion) {
+  console.log('Local storage is stale, using default values.');
+  var id = Math.random().toString().slice(2);
+
   view = 'setup';
+  rules = 'shing';
+  playerMap = {};
+  playerOrder = [id];
+  rounds = [{
+    hands: 1,
+    bids: {},
+    tricks: {}
+  }],
+  playerMap[id] = 'Player 1'
+  store.set('dataVersion', latestDataVersion);
+  store.set('view', view);
+  store.set('rules', rules);
+  store.set('playerMap', playerMap);
+  store.set('playerOrder', playerOrder);
+  store.set('rounds', rounds);
 }
 
-var ractive = new Ractive({
-  el: '#ractive-container',
 
-  template: '#template',
-
-  data: {
-    view: view,
-
-    players: players,
-
-    rules: rules,
-
-    ruleSets: {
-      regular: {
-        name: 'Regular',
-        details: [
-          '+1 point for each trick',
-          '+10 points for taking the exact number of tricks bid (for non-zero bids)',
-          '+5 points for taking zero tricks (for bids of zero)',
-        ],
-        scoreFunc: function(round, bid, tricks) {
-          // Avoid implicit conversion of null to 0.
-          if (bid === null || tricks === null) {
-            return null;
-          }
-          if (bid === tricks) {
-            if (bid > 0) {
-              return tricks + 10;
-            } else {
-              return tricks + 5;
-            }
-          } else {
-            return tricks;
-          }
-        }
-      },
-      alternate: {
-        name: 'Alternate',
-        details: [
-          '+1 point for each trick',
-          '+10 points for taking the exact number of tricks bid (including bids of zero)',
-          '-5 points for missing your bid',
-        ],
-        scoreFunc: function(round, bid, tricks) {
-          // Avoid implicit conversion of null to 0.
-          if (bid === null || tricks === null) {
-            return null;
-          }
-          if (bid === tricks) {
-            return tricks + 10;
-          } else {
-            return tricks - 5;
-          }
-        }
-      },
-      shing: {
-        name: "Shing's house rules",
-        details: [
-          '+1 point for each trick',
-          '+10 points for taking the exact number of tricks bid (for non-zero bids)',
-          'The greater of 5 points or the round number for taking zero tricks (for bids of zero)',
-          '-5 points for missing your bid',
-        ],
-        scoreFunc: function(round, bid, tricks) {
-          // Avoid implicit conversion of null to 0.
-          if (bid === null || tricks === null) {
-            return null;
-          }
-          if (bid === tricks) {
-            if (bid > 0) {
-              return tricks + 10;
-            } else {
-              // Successful zero bids get points equal to the maximum of 5
-              // or the current round number.
-              var points;
-              if ((round + 1) > 5) {
-                points = round + 1;
-              } else {
-                points = 5;
-              }
-              return points;
-            }
-          } else {
-            return tricks - 5;
-          }
-        }
-      }
-    },
-
-    // Compute data for the table.
-    createTable: function() {
-      var players = this.get('players');
-      var numRounds = this.get('numRounds');
-      var calcPoints = this.get('ruleSets')[this.get('rules')].scoreFunc;
-      var table = {};
-      table.rounds = [];
-      table.scores = [];
-      // The total score for each player is initially set to zero.
-      table.scores = players.map(function() {return 0});
-      // Create player-wise bids, tricks and points for each round.
-      for (var round = 0; round < numRounds; round++) {
-        var bids = [];
-        var tricks = [];
-        var pointsArray = [];
-        var totalBids = 0;
-        var totalTricks = 0;
-        // Retrieve each player's bid and tricks for the round and calculate points.
-        for (var index = 0; index < players.length; index++) {
-          var bid = players[index].bids[round];
-          var trick = players[index].tricks[round];
-          var points = calcPoints(round, bid, trick);
-          bids.push(bid);
-          tricks.push(trick);
-          pointsArray.push(points);
-          // Add points to the player's score (null points will be converted
-          // to 0 - not a drama).
-          table.scores[index] += points;
-          totalBids += bid;
-          totalTricks += trick;
-        }
-        table.rounds.push({
-          bids: bids,
-          tricks: tricks,
-          points: pointsArray,
-          totalBids: totalBids,
-          totalTricks: totalTricks,
-        });
-      }
-      return table;
-    }
-  },
-
-  computed: {
-    playerNames: function() {
-      return this.get('players').map(function(player) {
-        return player.name;
-      });
-    },
-
-    shortenedNames: function() {
-      return uniquePrefixes(this.get('playerNames'));
-    },
-
-    // Number of rounds, calculated from the first player's bids.
-    numRounds: function() {
-      return this.get('players')[0].bids.length;
-    }
+/**
+ * Return an integer if a base-10 integer could be parsed from string.
+ */
+function parseIntegerString(string) {
+  var integer = parseInt(string, 10);
+  if (!isNaN(integer)) {
+    return integer;
   }
-});
-
-ractive.on({
-  addPlayer: function(event) {
-    // The new player's bids and tricks arrays are filled with as many nulls
-    // as there are rounds.
-    var numRounds = this.get('numRounds');
-    var bids = [];
-    for (var i = 0; i < numRounds; i++) {
-      bids.push(null);
-    }
-    // Tricks is a copy of bids.
-    var tricks = bids.slice();
-    this.push('players', {
-      name: event.node.value.trim(),
-      bids: bids,
-      tricks: tricks
-    });
-    // Clear the new player input.
-    event.node.value = '';
-  },
-
-  removePlayer: function(event, index) {
-    this.splice('players', index, 1);
-  },
-
-  editPlayerName: function(event, index) {
-    this.set('players[' + index + '].name', event.node.value.trim());
-  },
-
-  editBid: function(event, round, index) {
-    var parsed = parseString(event.node.value.trim());
-    this.set('players[' + index + '].bids[' + round + ']', parsed);
-  },
-
-  editTrick: function(event, round, index) {
-    var parsed = parseString(event.node.value.trim());
-    this.set('players[' + index + '].tricks[' + round + ']', parsed);
-  },
-
-  addRound: function(event) {
-    // Adding a round pushes a null on to all players' bids and tricks arrays.
-    for (var index = 0; index < this.get('players').length; index++) {
-      this.push('players[' + index + '].bids', null);
-      this.push('players[' + index + '].tricks', null);
-    }
-  },
-
-  removeRound: function(event, round) {
-    for (var index = 0; index < this.get('players').length; index++) {
-      this.splice('players[' + index + '].bids', round, 1);
-      this.splice('players[' + index + '].tricks', round, 1);
-    }
-  },
-
-  removeAllRounds: function(event) {
-    // Set all player's bids and tricks arrays to an array with a single null.
-    for (var index = 0; index < this.get('players').length; index++) {
-      this.set('players[' + index + '].bids', [null]);
-      this.set('players[' + index + '].tricks', [null]);
-    }
-  }
-});
-
-// If storage is enabled, observe for change and write to storage.
-if (store.enabled) {
-  ractive.observe({
-    players: function() {
-      store.set('players', this.get('players'));
-    },
-
-    rules: function() {
-      store.set('rules', this.get('rules'));
-    },
-
-    view: function() {
-      store.set('view', this.get('view'));
-    }
-  }, {
-    // Don't call the functions initially.
-    init: false
-  });
 }
 
-// Return the shortest unique prefixes from an array of strings.
+
+/**
+ * Return true if value is an integer.
+ */
+function isInteger(value) {
+  return typeof value === "number" && isFinite(value) && Math.floor(value) === value;
+};
+
+
+/**
+ * Return the shortest unique prefixes of an array of strings.
+ * strings - array of strings
+ */
 function uniquePrefixes(strings) {
   return strings.map(function(string, index, strings) {
     // Create incrementally longer prefixes of string.
@@ -286,8 +84,342 @@ function uniquePrefixes(strings) {
   });
 }
 
-// Return a parsed base-10 integer or null if parse result was NaN.
-function parseString(string) {
-  var parsed = parseInt(string, 10);
-  return isNaN(parsed) ? null : parsed;
+
+/**
+ * Predict the number of hands for the next round given the previous two rounds' hands.
+ * The prediction behaves as follows:
+ *  - One more than last round if there has only been one round.
+ *  - One more than last round if hands are increasing,
+ *    but one less if there were 10 hands last round.
+ *  - One less than last round if hands are decreasing
+ *    and there was more than one hand last round.
+ *  - Same as last round otherwise.
+ * last - hands in the last round
+ * secondLast - hands in the second last round, or null if no second last round
+ * Returns the predicted number of hands.
+ */
+function predictHands(last, secondLast) {
+  // Guess one more than the last if there has only been one round.
+  if (secondLast === undefined) {
+    return last + 1;
+  }
+
+  if (last > secondLast) {
+    // If the number of hands is increasing...
+    if (last === 10) {
+      // ...but there were 10 hands last round, guess one less than last.
+      return last - 1;
+      // ...guess one more than last.
+    } else {
+      return last + 1;
+    }
+  }
+
+  // Guess one less than the last if hands are decreasing and there was more than
+  // one hand last round..
+  if (last < secondLast && last > 1) {
+      return last - 1;
+  }
+
+  // Otherwise guess the same as the last number of hands.
+  return last;
+}
+
+
+Ractive.DEBUG = false;
+
+var ractive = new Ractive({
+  el: '#ractive-container',
+  template: '#template',
+  data: {
+    view: view,
+    rules: rules,
+    playerMap: playerMap,
+    playerOrder: playerOrder,
+    rounds: rounds,
+
+    /**
+     * Sum a round's bids or tricks.
+     * index - round index
+     * key - key to sum ('bids' or 'tricks')
+     */
+    sumRound: function(index, key) {
+      var values = this.get('rounds.' + index + '.' + key),
+        sum = null;
+
+      this.get('playerOrder').forEach(function(id) {
+        if (values[id] != null) {
+          sum += values[id];
+        }
+      });
+      return sum;
+    },
+    rulesSet: {
+      regular: {
+        name: 'Regular',
+        details: [
+          '+1 point for each trick',
+          '+10 points for a successful non-zero bid',
+          '+5 points for a successful zero bid',
+        ],
+        calculatePoints: function(bid, tricks) {
+          if (bid === tricks) {
+            // Successful...
+            if (bid > 0) {
+              // ...non-zero bid.
+              return tricks + 10;
+            } else {
+              // ...zero bid
+              return tricks + 5;
+            }
+          } else {
+            // Unsuccessful bid.
+            return tricks;
+          }
+        }
+      },
+      alternate: {
+        name: 'Alternate',
+        details: [
+          '+1 point for each trick',
+          '+10 points for a successful bid',
+          '-5 points for an unsuccessful bid',
+        ],
+        calculatePoints: function(bid, tricks) {
+          if (bid === tricks) {
+            // Successful bid.
+            return tricks + 10;
+          } else {
+            // Unsuccessful bid.
+            return tricks - 5;
+          }
+        }
+      },
+      shing: {
+        name: "Shing's house rules",
+        details: [
+          '+1 point for each trick',
+          '+10 points for a successful non-zero bid)',
+          'Greater of the number of hands or 5 points for a successful zero bid',
+          '-5 points for an unsuccessful bid',
+        ],
+        calculatePoints: function(bid, tricks, hands) {
+          if (bid === tricks) {
+            // Successful...
+            if (bid > 0) {
+              // ...non-zero bid.
+              return tricks + 10;
+            } else {
+              // ...zero bid.
+              // Return the greater of the number of hands or 5.
+              if (hands > 5) {
+                return hands;
+              } else {
+                return 5;
+              }
+            }
+          } else {
+            // Unsuccessful bid.
+            return tricks - 5;
+          }
+        }
+      }
+    }
+  },
+  computed: {
+    players: function() {
+      var playerMap = this.get('playerMap'),
+        playerOrder = this.get('playerOrder'),
+        shortNames = uniquePrefixes(playerOrder.map(function(id) {
+          return playerMap[id];
+        }));
+
+      return playerOrder.map(function(id, index) {
+        return {
+          id: id,
+          name: playerMap[id],
+          shortName: shortNames[index]
+        };
+      });
+    },
+
+    /**
+     * Calculate points if bid and tricks don't equate to null.
+     * bid - Player's bid
+     * tricks - Player's tricks
+     * hands - Number of hands this round
+     */
+    calculatePoints: function() {
+      var calculatePoints = this.get('rulesSet')[this.get('rules')].calculatePoints,
+        calculatePointsWrapper = function(bid, tricks, hands) {
+          if (bid != null && tricks != null) {
+            return calculatePoints(bid, tricks, hands);
+          }
+        };
+
+      return calculatePointsWrapper;
+    },
+
+    /**
+     * Return a object of player scores (sum of points).
+     */
+    scores: function() {
+      var rounds = this.get('rounds'),
+        playerOrder = this.get('playerOrder'),
+        calculatePoints = this.get('calculatePoints'),
+        scores = {};
+
+      // Calculate the score for each player.
+      playerOrder.forEach(function(id) {
+        var score = null;
+
+        // Score is the sum a player's points for each round.
+        rounds.forEach(function(round) {
+          var points = calculatePoints(
+            round['bids'][id], round['tricks'][id], round['hands']);
+
+          if (points != null) {
+            score += points;
+          }
+        });
+
+        if (score != null) {
+          scores[id] = score;
+        }
+      });
+      return scores;
+    }
+  }
+});
+
+ractive.on({
+  /**
+   * Map unique (hopefully) id to player name, append id to player order array.
+   */
+  addPlayer: function(event) {
+    var name = event.node.value.trim(),
+      // players = this.get('players'),
+      //playerOrder = this.get('playerOrder'),
+      id = Math.random().toString().slice(2);
+
+    this.set('playerMap.' + id, name);
+    this.push('playerOrder', id);
+    // Clear the new player input field.
+    event.node.value = '';
+  },
+
+  /**
+   * Remove player from players map and player order array.
+   * The player's bids and tricks are removed from each round.
+   * id - id of player to remove
+   */
+  removePlayer: function(event, id) {
+    var playerMap = this.get('playerMap'),
+      playerOrder = this.get('playerOrder'),
+      rounds = this.get('rounds');
+
+    delete playerMap[id];
+    playerOrder.splice(playerOrder.indexOf(id), 1);
+    this.set({playerMap: playerMap, playerOrder: playerOrder});
+
+    // Remove player properties from each round's bids and tricks.
+    rounds.forEach(function(round, index) {
+      var bids = this.get('rounds.' + index + '.bids.' + id),
+        tricks = this.get('rounds.' + index + '.tricks.' + id);
+
+      delete bids[id];
+      delete tricks[id];
+      this.set('rounds.' + index + '.bids.' + id, bids);
+      this.set('rounds.' + index + '.tricks.' + id, tricks);
+    });
+  },
+
+  /**
+   * Change the player's name to the input value.
+   * id - id of player
+   */
+  editPlayerName: function(event, id) {
+    var name = event.node.value.trim();
+
+    this.set('playerMap.' + id, name);
+  },
+
+  /**
+   * Add a new round to the rounds array. Predict the number of hands in the
+   * next round.
+   */
+  addRound: function(event) {
+    var rounds = this.get('rounds'),
+      lastRound = rounds[rounds.length - 1],
+      secondLastRound = rounds[rounds.length - 2];
+
+    this.push('rounds', {
+      hands: predictHands(lastRound.hands, secondLastRound && secondLastRound.hands),
+      bids: {},
+      tricks: {}
+    });
+  },
+
+  /**
+   * Remove round from a rounds array.
+   * index - index of round
+   */
+  removeRound: function(event, index) {
+    this.splice('rounds', index, 1);
+  },
+
+  /**
+   * Replace all rounds with a default first round.
+   */
+  removeAllRounds: function(event) {
+    this.splice('rounds', 0, this.get('rounds').length, {
+      hands: 1,
+      bids: {},
+      tricks: {}
+    });
+  },
+
+  /**
+   * Change player's bid to input value for given round.
+   * index - index of round
+   * id - player id
+   */
+  editBid: function(event, index, id) {
+    var integer = parseIntegerString(event.node.value.trim());
+    this.set('rounds.' + index + '.bids.' + id, integer);
+  },
+
+  /**
+   * Change player's tricks to input value for given round.
+   * index - index of round
+   * id - player id
+   */
+  editTrick: function(event, index, id) {
+    var integer = parseIntegerString(event.node.value.trim());
+    this.set('rounds.' + index + '.tricks.' + id, integer);
+  }
+});
+
+
+if (store.enabled) {
+  ractive.observe({
+    view: function() {
+      store.set('view', this.get('view'));
+    },
+    rules: function() {
+      store.set('rules', this.get('rules'));
+    },
+    playerMap: function() {
+      store.set('playerMap', this.get('playerMap'));
+    },
+    playerOrder: function() {
+      store.set('playerOrder', this.get('playerOrder'));
+    },
+    rounds: function() {
+      store.set('rounds', this.get('rounds'));
+    }
+  }, {
+    // Don't call the functions initially.
+    init: false
+  });
 }
